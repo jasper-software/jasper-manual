af85dfbfe0ce9a2f5810d0f39eb69886289aeec7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-05 15:02:39 +0000

    Deploy jasper-software/jasper-manual to github.com/jasper-software/jasper-manual.git:gh-pages

--- search/all_6.js
+++ search/all_6.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['reference_20manual_20_28version_202_2e0_2e19_29',['Reference Manual (Version 2.0.19)',['../index.html',1,'']]]
+  ['reference_20manual_20_28version_202_2e0_2e20_29',['Reference Manual (Version 2.0.20)',['../index.html',1,'']]]
 ];
