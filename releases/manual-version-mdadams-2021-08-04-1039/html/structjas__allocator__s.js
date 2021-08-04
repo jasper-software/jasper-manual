86d14c3548a863f2eda03a528263b401ae150342
Michael Adams <mdadams@ece.uvic.ca>
2021-08-04 17:40:24 +0000

    Deploying release manual-version-mdadams-2021-08-04-1039

---
+++ releases/manual-version-mdadams-2021-08-04-1039/html/structjas__allocator__s.js
@@ -0,0 +1,8 @@
+var structjas__allocator__s =
+[
+    [ "reserved", "structjas__allocator__s.html#ac77a5b7030d36d201c60991eacca2d86", null ],
+    [ "alloc", "structjas__allocator__s.html#a7614a320fe50cf68b048bdc2b5873732", null ],
+    [ "cleanup", "structjas__allocator__s.html#a55f8f44a42e0bb6c9cf410fcabe12ef4", null ],
+    [ "free", "structjas__allocator__s.html#a04e4df44486bb85c5ea113466d6947da", null ],
+    [ "realloc", "structjas__allocator__s.html#ab1c004d0b27abd012963e5b094ea58c3", null ]
+];
