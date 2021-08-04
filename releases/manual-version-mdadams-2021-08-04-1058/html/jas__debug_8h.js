3ac555461a9323d8e80af43bcd752446ecaf1f2d
Michael Adams <mdadams@ece.uvic.ca>
2021-08-04 17:59:42 +0000

    Deploying release manual-version-mdadams-2021-08-04-1058

---
+++ releases/manual-version-mdadams-2021-08-04-1058/html/jas__debug_8h.js
@@ -0,0 +1,9 @@
+var jas__debug_8h =
+[
+    [ "JAS_STRINGIFY", "jas__debug_8h.html#ac41b6ecee0f1084c4961c79e3cb89e93", null ],
+    [ "JAS_STRINGIFYX", "jas__debug_8h.html#a1cfc9aea772cf8912eb87f3aeb6a0c48", null ],
+    [ "jas_deprecated", "jas__debug_8h.html#acc19fdad02a8fd8ddd5c6be5ffbc8c49", null ],
+    [ "jas_memdump", "jas__debug_8h.html#a87cbffcffed65a714c67884058416029", null ],
+    [ "jas_veprintf_discard", "jas__debug_8h.html#afaa681af0c8b9fb5daa5051fb9146858", null ],
+    [ "jas_veprintf_stderr", "jas__debug_8h.html#a27ba679623268c485725a8319041b3d9", null ]
+];
---
+++ releases/manual-version-mdadams-2021-08-04-1058/html/search/variables_7.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['realloc_308',['realloc',['../structjas__allocator__s.html#ab1c004d0b27abd012963e5b094ea58c3',1,'jas_allocator_s']]]
+];
