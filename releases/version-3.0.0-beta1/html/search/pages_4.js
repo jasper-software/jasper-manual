0ba56a5e21ab5e0ca3ea0b1f484d89fcb9c81ef2
Michael Adams <mdadams@ece.uvic.ca>
2022-02-05 20:33:21 +0000

    Deploying release version-3.0.0-beta1

---
+++ releases/version-3.0.0-beta1/html/search/pages_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['jasper_20application_20programs_713',['JasPer Application Programs',['../apps.html',1,'']]],
+  ['jasper_20library_714',['JasPer Library',['../lib.html',1,'']]]
+];
