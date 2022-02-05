80e1f55f9a9138e0a84f5c0726b201aa6e8293e2
Michael Adams <mdadams@ece.uvic.ca>
2022-02-05 21:05:53 +0000

    Deploying release version-3.0.0-rc2

---
+++ releases/version-3.0.0-rc2/html/search/pages_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['bug_20tracking_20and_20reporting_704',['Bug Tracking and Reporting',['../bugs.html',1,'']]],
+  ['bibliography_705',['Bibliography',['../citelist.html',1,'']]]
+];
