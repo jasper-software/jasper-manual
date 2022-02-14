ec827bc78163472789c8baedb6c6d685f16cd735
Michael Adams <mdadams@ece.uvic.ca>
2022-02-14 14:56:10 +0000

    Deploying release version-3.0.2

---
+++ releases/version-3.0.2/html/search/pages_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['codecs_706',['Codecs',['../codecs.html',1,'']]],
+  ['configuration_2c_20initialization_2c_20and_20shutdown_707',['Configuration, Initialization, and Shutdown',['../lib_init.html',1,'lib']]]
+];
