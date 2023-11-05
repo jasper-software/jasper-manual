03f744d2003e9982f8c4b327562d4cac74215736
Michael Adams <mdadams@ece.uvic.ca>
2023-11-05 06:25:36 +0000

    Deploying release version-4.1.0

---
+++ releases/version-4.1.0/html/group__module__tvp.js
@@ -0,0 +1,12 @@
+var group__module__tvp =
+[
+    [ "jas_taginfo_t", "structjas__taginfo__t.html", null ],
+    [ "jas_tvparser_t", "structjas__tvparser__t.html", null ],
+    [ "jas_taginfo_nonull", "group__module__tvp.html#gad66c6fe65a0d31169580c22c7fce2844", null ],
+    [ "jas_taginfos_lookup", "group__module__tvp.html#ga45ca405b9b7cb0502334a6079111c254", null ],
+    [ "jas_tvparser_create", "group__module__tvp.html#gafdccfb1f4d7bc5a3256148fb5b4d619d", null ],
+    [ "jas_tvparser_destroy", "group__module__tvp.html#ga52e5862d4c19f4f183b6658a79c6bdd7", null ],
+    [ "jas_tvparser_gettag", "group__module__tvp.html#ga5d8492c068dd09fef6d682f89c72fa62", null ],
+    [ "jas_tvparser_getval", "group__module__tvp.html#ga06082ba05e3989e24fd87068ba27f65b", null ],
+    [ "jas_tvparser_next", "group__module__tvp.html#gab74afb660bef195879afe385f73be12d", null ]
+];
---
+++ releases/version-4.1.0/html/search/all_2.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['cleanup_3',['cleanup',['../structjas__allocator__s.html#a55f8f44a42e0bb6c9cf410fcabe12ef4',1,'jas_allocator_s']]],
+  ['codecs_4',['Codecs',['../codecs.html',1,'']]],
+  ['color_20management_5',['Color Management',['../group__module__cm.html',1,'']]],
+  ['command_2dline_20interface_20_28cli_29_20option_20processing_6',['Command-Line Interface (CLI) Option Processing',['../group__module__getopt.html',1,'']]],
+  ['configuration_2c_20initialization_2c_20and_20shutdown_7',['Configuration, Initialization, and Shutdown',['../lib_init.html',1,'lib'],['../group__module__init.html',1,'(Global Namespace)']]]
+];
