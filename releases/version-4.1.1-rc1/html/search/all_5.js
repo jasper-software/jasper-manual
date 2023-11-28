3484a6f6c16bfb44bfe4817cdc086b0d18eae288
Michael Adams <mdadams@ece.uvic.ca>
2023-11-28 17:22:07 +0000

    Deploying release version-4.1.1-rc1

---
+++ releases/version-4.1.1-rc1/html/search/all_5.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['fixed_2dpoint_20arithmetic_14',['Fixed-Point Arithmetic',['../group__module__fixed.html',1,'']]],
+  ['free_15',['free',['../structjas__allocator__s.html#a9258433f3856e3c7ba8f7a831bc58b61',1,'jas_allocator_s']]]
+];
---
+++ releases/version-4.1.1-rc1/html/search/pages_3.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['i_2fo_20streams_706',['I/O Streams',['../lib_iostreams.html',1,'lib']]],
+  ['images_2c_20sequences_2c_20and_20color_20management_707',['Images, Sequences, and Color Management',['../lib_images.html',1,'lib']]],
+  ['installation_708',['Installation',['../install.html',1,'']]],
+  ['introduction_709',['Introduction',['../intro.html',1,'']]]
+];
