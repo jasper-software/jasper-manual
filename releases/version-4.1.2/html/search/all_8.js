9c081707fd58c2b0914e5032135b6f076ed5076f
Michael Adams <mdadams@ece.uvic.ca>
2024-01-11 15:16:04 +0000

    Deploying release version-4.1.2

---
+++ releases/version-4.1.2/html/search/all_8.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['license_385',['License',['../license.html',1,'']]],
+  ['logging_386',['Logging',['../lib_log.html',1,'lib'],['../group__module__log.html',1,'(Global Namespace)']]]
+];
