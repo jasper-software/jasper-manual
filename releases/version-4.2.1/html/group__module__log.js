0a7f0e80442112318de227689abababa6e1fddcc
Michael Adams <mdadams@ece.uvic.ca>
2024-02-19 19:21:17 +0000

    Deploying release version-4.2.1

---
+++ releases/version-4.2.1/html/group__module__log.js
@@ -0,0 +1,16 @@
+var group__module__log =
+[
+    [ "jas_logtype_t", "structjas__logtype__t.html", null ],
+    [ "JAS_LOGTYPE_CLASS_DEBUG", "group__module__log.html#ga70ef8ace1c49840422a2d2eed5dd5cd7", null ],
+    [ "JAS_LOGTYPE_CLASS_ERROR", "group__module__log.html#ga30a21be0a51e17b15af951d49df6c6fe", null ],
+    [ "JAS_LOGTYPE_CLASS_INFO", "group__module__log.html#ga1db4b74642d7674509da52b164f0e5a8", null ],
+    [ "JAS_LOGTYPE_CLASS_NULL", "group__module__log.html#ga331c41dece69cfcaa40ede386f1634c7", null ],
+    [ "JAS_LOGTYPE_CLASS_WARN", "group__module__log.html#gaaeab8fc244d54e5c812e4b3eb6a4ca8f", null ],
+    [ "jas_vlogmsgf_t", "group__module__log.html#ga2d03821b5abeabc089595094761aa908", null ],
+    [ "jas_logtype_getclass", "group__module__log.html#ga7dc5eff5dcd7b224109d9508daa8a82a", null ],
+    [ "jas_logtype_getpriority", "group__module__log.html#gaaf72c5f6022dd20177880889d5028528", null ],
+    [ "jas_logtype_init", "group__module__log.html#ga833016f5f1ffae90856f0b8fbbddd0c4", null ],
+    [ "jas_vlogmsgf", "group__module__log.html#gafd75d5af7e90b2b444238b534907e417", null ],
+    [ "jas_vlogmsgf_discard", "group__module__log.html#ga4fc2046793797bf03d473d407fa4bb5d", null ],
+    [ "jas_vlogmsgf_stderr", "group__module__log.html#ga9b7841528e562f7bb0b207ad4a70eb38", null ]
+];
